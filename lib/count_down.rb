1a41ff6d4cda2342dcd6e4b4ab597d8ddaef7606
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-07 17:14:21 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,8 +1,8 @@
-$count=l0 
-while $count>=1 do 
-  puts $count
-  $count-=1
-  if $count==1 
+count=l0 
+while count>=1 do 
+  puts count
+  count-=1
+  if count==1 
   puts "Happy New Year!"
 end   
 end 
